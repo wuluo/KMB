861178c3e6e5a963f3dac7e16d34826b998d693d
wujing01 <wujing01@gomeplus.com>
2017-05-23 18:09:50 +0800

    update

---
+++ Taurasi/application/classes/Controller/Blog.php
@@ -0,0 +1,10 @@
+<?php
+
+class Controller_Blog extends Controller_Base {
+    public function action_index()
+    {
+
+		$this->display();
+    }
+
+}
---
+++ Taurasi/application/classes/Controller/Role.php
@@ -0,0 +1,10 @@
+<?php
+
+class Controller_Role extends Controller_Base {
+    public function action_index()
+    {
+
+		$this->display();
+    }
+
+}
